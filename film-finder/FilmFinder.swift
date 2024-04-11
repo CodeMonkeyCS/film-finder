a1b2bf1534598c4ec33f49011fe574af42ddf366
Cameron Small <csmall@aegistg.com>
2024-04-10 21:20:33 -0500

    Working on converting the GET requests to Swift.

--- film-finder/FilmFinder.swift
+++ film-finder/FilmFinder.swift
@@ -7,6 +7,102 @@
 
 import Foundation
 
+struct Movies: Codable {
+    let items: [MovieInfo]
+}
+
+struct MovieInfo: Codable {
+    let id: String
+}
+
+class FilmFinder {
+    let tmdbBaseUrl = "https://api.themoviedb.org/3"
+    var tmdbKey = ""
+    
+    init() {
+        
+        if let value = ProcessInfo.processInfo.environment["TMDB_KEY"] {
+            tmdbKey = value
+        } else {
+            print("TMDB_KEY environment variable not set.")
+        }
+    }
+    
+    func getGenres() async {
+        let genreRequestEndpoint = "/genre/movie/list"
+        let requestParams = "?api_key=\(self.tmdbKey)"
+        let urlToFetch = URL(string: tmdbBaseUrl + genreRequestEndpoint + requestParams)
+        
+        var request = URLRequest(url: urlToFetch!,timeoutInterval: Double.infinity)
+        request.httpMethod = "GET"
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+          guard let data = data else {
+            print(String(describing: error))
+            return
+          }
+          print(String(data: data, encoding: .utf8)!)
+        }
+
+        task.resume()
+
+//        let wrapper = try JSONDecoder().decode(Wrapper.self, from: data)
+//        return wrapper.items[0]
+    }
+    
+    func getMovies() async {
+        let selectedGenre = "" //getSelectedGenre();
+        let requestParams = "?api_key=\(self.tmdbKey)&with_genres=\(selectedGenre)"
+        let discoverMovieEndpoint = "/discover/movie"
+        let urlToFetch = URL(string: tmdbBaseUrl + discoverMovieEndpoint + requestParams)
+        
+        var request = URLRequest(url: urlToFetch!,timeoutInterval: Double.infinity)
+        request.httpMethod = "GET"
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+          guard let data = data else {
+            print(String(describing: error))
+            return
+          }
+          print(String(data: data, encoding: .utf8)!)
+        }
+
+        task.resume()
+    }
+    
+    func getMovieInfo(movie: MovieInfo) async {
+        let movieId = movie.id;
+        let movieEndpoint = "/movie/\(movieId)"
+        let requestParams = "?api_key=\(tmdbKey)"
+        let urlToFetch = URL(string: tmdbBaseUrl + movieEndpoint + requestParams)
+        
+        var request = URLRequest(url: urlToFetch!,timeoutInterval: Double.infinity)
+        request.httpMethod = "GET"
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+          guard let data = data else {
+            print(String(describing: error))
+            return
+          }
+          print(String(data: data, encoding: .utf8)!)
+        }
+
+        task.resume()
+    }
+    
+    /// Gets a list of movies and ultimately displays the info of a random movie from the list
+    func showRandomMovie() async {
+//        const movieInfo = document.getElementById('movieInfo');
+//        if (movieInfo.childNodes.length > 0) {
+//            clearCurrentMovie();
+//        };
+//        const movies = await getMovies();
+//        const randomMovie = getRandomMovie(movies);
+//        const info = await getMovieInfo(randomMovie);
+//        displayMovie(info);
+    }
+}
+
 //const tmdbKey = ''; - switch to use an environment variable
 //const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 //const playBtn = document.getElementById('playBtn');
